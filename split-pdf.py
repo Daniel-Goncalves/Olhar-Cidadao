c4fceea8a9fbc0dae25fb100b43b53b47ed99931
Daniel-goncalves <dani.pereira47@gmail.com>
2018-05-09 17:53:36 -0300

    Split pdf fixed. Small changes in the script in order to make it more efficient

--- split-pdf.py
+++ split-pdf.py
@@ -17,36 +17,33 @@ for i in xrange(inputpdf.numPages):
 
 """
 
+
+
 #Get all .pdf files of the path
 
-list=[]
+pdfs_to_be_split_list=[]
 
-directory="pdf"
+directory="./pdf"
 
 for root,dirs,files in os.walk(directory):
     for filename in files:
         if filename.endswith('.pdf'):
             t=os.path.join(directory,filename)
-            list.append(t)
+            pdfs_to_be_split_list.append(t)
+            ### APAGAR t
 
-m=len(list)
-print(list[0])
-i=0
 
-#Select one PDF file and create a lot of others PDF files into a new path
-for i in xrange(m):
-	print(list[i])
-	ofolder = list[i]
-	nfolder = ofolder.replace(".", "")
-	
-	if not os.path.exists(nfolder):
-		os.mkdir(nfolder)
-	name = ofolder
-	#print(name)
-	#print(nfolder)
-	inputpdf = PdfFileReader(open(name, "rb"))
+#Select one PDF file and create a lot of others PDF files into pdf folder
+for i in range(0,len(pdfs_to_be_split_list)):
+
+	filePath = pdfs_to_be_split_list[i]
+	filename = filePath.split("/")[-1:][0][:-4]
+
+
+	inputpdf = PdfFileReader(open(filePath, "rb"))
 	for j in xrange(inputpdf.numPages):
 		output = PdfFileWriter()
 		output.addPage(inputpdf.getPage(j))
-		with open("%s/document-page%d.pdf" % (nfolder, j), "wb") as outputStream:
+		with open("pdf/" + filename +str(j)  +".pdf", "wb") as outputStream:
 			output.write(outputStream)
+	
